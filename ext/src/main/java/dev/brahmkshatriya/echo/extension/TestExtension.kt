bee3471b7d6e168e1398702718c795c0b8d8b96e
m-tki <m_tki@outlook.com>
2025-07-01 10:23:37 +0300

    Implement support for iHeartRadio streams

--- ext/src/main/java/dev/brahmkshatriya/echo/extension/TestExtension.kt
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/TestExtension.kt
@@ -1,16 +1,213 @@
 package dev.brahmkshatriya.echo.extension
 
 import dev.brahmkshatriya.echo.common.clients.ExtensionClient
-import dev.brahmkshatriya.echo.common.settings.Setting
+import dev.brahmkshatriya.echo.common.clients.HomeFeedClient
+import dev.brahmkshatriya.echo.common.clients.TrackClient
+import dev.brahmkshatriya.echo.common.clients.RadioClient
+import dev.brahmkshatriya.echo.common.clients.SearchFeedClient
 import dev.brahmkshatriya.echo.common.settings.Settings
+import dev.brahmkshatriya.echo.common.models.EchoMediaItem.Companion.toMediaItem
+import dev.brahmkshatriya.echo.common.models.ImageHolder.Companion.toImageHolder
+import dev.brahmkshatriya.echo.common.models.Radio
+import dev.brahmkshatriya.echo.common.models.Album
+import dev.brahmkshatriya.echo.common.models.Artist
+import dev.brahmkshatriya.echo.common.models.EchoMediaItem
+import dev.brahmkshatriya.echo.common.models.Shelf
+import dev.brahmkshatriya.echo.common.models.Streamable
+import dev.brahmkshatriya.echo.common.models.Tab
+import dev.brahmkshatriya.echo.common.models.Track
+import dev.brahmkshatriya.echo.common.models.User
+import dev.brahmkshatriya.echo.common.models.Playlist
+import dev.brahmkshatriya.echo.common.models.QuickSearchItem
+import dev.brahmkshatriya.echo.common.models.Feed.Companion.toFeed
+import dev.brahmkshatriya.echo.common.models.Streamable.Source.Companion.toSource
+import dev.brahmkshatriya.echo.common.helpers.ClientException
+import dev.brahmkshatriya.echo.common.helpers.PagedData
+import dev.brahmkshatriya.echo.common.helpers.ContinuationCallback.Companion.await
+import dev.brahmkshatriya.echo.common.settings.Setting
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.json.Json
 
-class TestExtension : ExtensionClient {
+@Serializable
+data class Station(
+    val hits: List<Hit>,
+) {
+
+    @Serializable
+    data class Hit(
+        val id: Long,
+        val name: String,
+        val description: String,
+        val logo: String? = null,
+        val streams: Stream,
+    ) {
+
+        @Serializable
+        data class Stream(
+            @SerialName("secure_hls_stream")
+            val hls: String? = null,
+            @SerialName("secure_shoutcast_stream")
+            val shoutcast: String? = null,
+            @SerialName("secure_pls_stream")
+            val pls: String? = null,
+        )
+    }
+}
+
+@Serializable
+data class Genre(
+    val hits: List<Hit>,
+) {
+
+    @Serializable
+    data class Hit(
+        val id: Long,
+        val name: String,
+    )
+}
+
+@Serializable
+data class StationSearch(
+    val stations: List<Station>,
+) {
+
+    @Serializable
+    data class Station(
+        val id: Long,
+    )
+}
+
+class TestExtension : ExtensionClient, HomeFeedClient, TrackClient, RadioClient, SearchFeedClient {
     override suspend fun onExtensionSelected() {}
 
-    override val settingItems: List<Setting> = emptyList()
+    override val settingItems get() = emptyList<Setting>()
 
     private lateinit var setting: Settings
     override fun setSettings(settings: Settings) {
         setting = settings
     }
+
+    private val searchLink = "https://api.iheart.com/api/v1/catalog/searchStation/"
+    private val stationLink = "https://api.iheart.com/api/v2/content/liveStations/"
+    private val genreLink = "https://api.iheart.com/api/v2/content/genre/"
+
+    private val client by lazy { OkHttpClient.Builder().build() }
+    private suspend fun call(url: String) = client.newCall(
+        Request.Builder().url(url).build()
+    ).await().body.string()
+
+    private val json by lazy { Json { ignoreUnknownKeys = true } }
+    private inline fun <reified T> String.toData() =
+        runCatching { json.decodeFromString<T>(this) }.getOrElse {
+            throw IllegalStateException("Failed to parse JSON: $this", it)
+        }
+
+    private suspend fun parsePLS(stream: String?): String {
+        if (stream != null) {
+            val content = call(stream)
+            for (line in content.lines()) {
+                if (line.startsWith("File1=")) {
+                    return line.substring(6)
+                }
+            }
+        }
+        return ""
+    }
+
+    private fun getStream(streams: Station.Hit.Stream): String =
+        streams.shoutcast ?: streams.pls ?: ""
+
+    private fun getStreamType(streams: Station.Hit.Stream): Map<String, String> =
+        streams.shoutcast?.let { mapOf("type" to "shoutcast") }
+            ?: streams.pls?.let { mapOf("type" to "pls") } ?: mapOf()
+
+    private fun String.toShelf(): List<Shelf> {
+        return this.toData<Station>().hits.filter {
+                getStream(it.streams).isNotEmpty()
+            }.map {
+                Track(
+                    id = it.id.toString(),
+                    title = it.name,
+                    subtitle = it.description,
+                    description = it.description,
+                    cover = it.logo?.toImageHolder(),
+                    streamables = listOf(
+                        Streamable.server(
+                            getStream(it.streams),
+                            0,
+                            extras = getStreamType(it.streams)
+                        )
+                    )
+                ).toMediaItem().toShelf()
+            }
+    }
+
+    override fun getHomeFeed(tab: Tab?) = PagedData.Single {
+        call("$stationLink?&genreId=${tab!!.id}&limit=5000").toShelf()
+    }.toFeed()
+
+    override suspend fun getHomeTabs(): List<Tab> {
+        return call(genreLink).toData<Genre>().hits.map {
+            Tab(title = it.name, id = it.id.toString())
+        }
+    }
+
+    override fun getShelves(track: Track): PagedData<Shelf> {
+        return PagedData.empty()
+    }
+
+    override suspend fun loadStreamableMedia(
+        streamable: Streamable,
+        isDownload: Boolean
+    ): Streamable.Media {
+        val source = if (streamable.extras["type"] == "pls")
+            parsePLS(streamable.id) else streamable.id
+        return Streamable.Media.Server(
+            listOf(source.toSource()),
+            false
+        )
+    }
+
+    override suspend fun loadTrack(track: Track) = track
+    override fun loadTracks(radio: Radio) = PagedData.empty<Track>()
+    override suspend fun radio(track: Track, context: EchoMediaItem?) = Radio("", "")
+    override suspend fun radio(album: Album) = throw ClientException.NotSupported("Album radio")
+    override suspend fun radio(artist: Artist) = throw ClientException.NotSupported("Artist radio")
+    override suspend fun radio(user: User) = throw ClientException.NotSupported("User radio")
+    override suspend fun radio(playlist: Playlist) =
+        throw ClientException.NotSupported("Playlist radio")
+
+    override suspend fun deleteQuickSearch(item: QuickSearchItem) {}
+    override suspend fun quickSearch(query: String): List<QuickSearchItem> {
+        return emptyList()
+    }
+
+    private suspend fun String.toSearchShelf(): List<Shelf> =
+        this.toData<StationSearch>().stations.map { result ->
+            val station = call(stationLink + result.id).toData<Station>().hits[0]
+            Track(
+                id = station.id.toString(),
+                title = station.name,
+                subtitle = station.description,
+                description = station.description,
+                cover = station.logo?.toImageHolder(),
+                streamables = listOf(
+                    Streamable.server(
+                        getStream(station.streams),
+                        0,
+                        extras = getStreamType(station.streams)
+                    )
+                )
+            ).toMediaItem().toShelf()
+        }
+
+    override fun searchFeed(query: String, tab: Tab?) =
+        PagedData.Single {
+            call("$searchLink?&keywords=\"$query\"").toSearchShelf()
+        }.toFeed()
+
+    override suspend fun searchTabs(query: String) = emptyList<Tab>()
 }
